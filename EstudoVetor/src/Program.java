fbcd608081d998c35ff3cce983dc07c7a868dfd1
Gustavo <gustavoamalheiros@gmail.com>
2020-05-21 01:23:46 -0300

    Initial commit

---
+++ EstudoVetor/src/Program.java
@@ -0,0 +1,29 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Dê o número de pessoas:");
+		int numero = sc.nextInt();
+		
+		double[] altura = new double[numero];
+		
+		
+		for(int i = 0; i<numero; i++) {
+			System.out.println("Digite a altura da pessoa na posição" + i);
+			altura[i] = sc.nextDouble();
+			
+		}
+		for(int i =0; i<altura.length; i++) {
+			System.out.println(altura[i]);
+		}
+		
+		sc.close();
+	}
+
+}
---
+++ VetorAula89/src/application/Program.java
@@ -0,0 +1,42 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Register;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		Register[] cadastro = new Register[10];
+		
+		//Reserves quantity
+		int n = sc.nextInt();
+		sc.nextLine();
+		
+		for(int i=0; i<n; i++) {
+			System.out.println("Name: ");
+			String name = sc.nextLine();
+			System.out.println("Email: ");
+			String email = sc.nextLine();
+			System.out.println("Room that you choose: 0 to 9");
+			int room = sc.nextInt();
+			sc.nextLine();
+			
+			cadastro[room] = new Register(name, email);	
+		}
+		
+		for(int i=0; i<10; i++) {
+			if(cadastro[i]!=null) {
+				System.out.println("Room " + i + ": "+ cadastro[i]);
+				
+			}
+		}
+		sc.close();
+	}
+
+}
